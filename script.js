a772f9584da5dbfeac72b3a6ca3c668fae075f11
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-02-16 17:03:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var header = document.querySelector('header');
-var gallery = document.querySelector('#gallery');
-
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    header.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-    gallery.style.transform = 'translateX(' + (-100 + scrollPosition * 0.5) + 'px)';
-});
 
 new fullpage('#fullpage', {
 	anchors: ['header', 'map', 'gallery', 'buttons'],
