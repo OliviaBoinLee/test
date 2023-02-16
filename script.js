edc21437f2267a6b88e16fd20acad0a58303fc0b
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-02-16 19:11:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 
-new fullpage('#fullpage', {
-	anchors: ['header', 'map', 'gallery', 'buttons'],
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
-	menu: '#menu',
-	scrollBar: true,
-	autoScrolling: true,
-    fitToSection: true,
-    afterLoad: function(origin, destination, direction){
-        var loadedSection = destination.item;
-        var sectionId = loadedSection.getAttribute('id');
-
-        if (sectionId === 'buttons') {
-            var buttons = document.querySelectorAll('#buttons button');
-            buttons.forEach(function(button) {
-                button.addEventListener('click', function() {
-                    alert('Button ' + button.getAttribute('id') + ' clicked');
-                });
-            });
-        }
-    }
-});
-
 var target = document.querySelectorAll('.btn_open');
 var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
 var targetID;
@@ -39,3 +17,12 @@ for(var j = 0; j < target.length; j++){
     this.parentNode.parentNode.style.display = 'none';
   });
 }
+
+new fullScroll({
+     // current position
+    currentPosition: 0,
+    // display dots navigation
+    displayDots: true,
+    // where to place the dots navigation
+    dotsPosition: 'left'
+});
