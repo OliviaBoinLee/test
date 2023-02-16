84a72d759d7ac98cd547b86959ba1bd74fad039a
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-02-16 15:31:34 +0900

    Create animation.js

---
+++ animation.js
@@ -0,0 +1,31 @@
+<!-- header -->
+<header class="scroll-in">
+  <img src="header-image.jpg" alt="Header Image">
+  <div class="text-box">
+    <h1>Welcome to Our Wedding</h1>
+    <p>November 30, 2023</p>
+  </div>
+</header>
+
+<!-- map -->
+<section id="map" class="scroll-in">
+  <div id="map-canvas"></div>
+</section>
+
+<!-- gallery -->
+<section id="gallery" class="scroll-in">
+  <div class="gallery-container">
+    <img src="gallery-image-1.jpg" alt="Gallery Image 1">
+    <img src="gallery-image-2.jpg" alt="Gallery Image 2">
+    <img src="gallery-image-3.jpg" alt="Gallery Image 3">
+    <img src="gallery-image-4.jpg" alt="Gallery Image 4">
+    <img src="gallery-image-5.jpg" alt="Gallery Image 5">
+    <img src="gallery-image-6.jpg" alt="Gallery Image 6">
+  </div>
+</section>
+
+<!-- buttons -->
+<section id="buttons" class="scroll-in">
+  <button>RSVP Now</button>
+  <button>View Our Registry</button>
+</section>
